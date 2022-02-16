4cd70da26e780c6c5c4e073b6a5573e3b58a5a87
JanSamuel <jan.samuel.o@gmail.com>
2022-02-16 11:35:59 +0100

    Changes in calculating hand strength
    Fixed Full strength
    Added high card
    Removed no needed Royal poker

--- texas/values.py
+++ texas/values.py
@@ -2,6 +2,16 @@ from functools import partial
 
 from .cards import figures
 
+def is_high_card(figure, hand):
+    """
+    >>> is_high_card("K", ["Ka", "3a", "6a", "4a"])
+    True
+    >>> is_high_card("3", ["Ka", "3a", "6a", "4a"])
+    False
+    """
+    hand_value = sorted([figures.index(cards[:-1]) for cards in hand])
+    return figure == figures[max(hand_value)]
+
 def is_pair(figure, hand):
     """
     >>> is_pair("3", ["3a", "3a", "6a", "4a"])     
@@ -11,7 +21,7 @@ def is_pair(figure, hand):
     >>> is_pair("3", ["Ka", "4a", "6a", "3b"])     
     False
     """
-    return len([x for x in hand if x[:-1] == figure]) >=2
+    return len([x for x in hand if x[:-1] == figure]) == 2
 
 def is_two_pair(figure1, figure2, hand):
     """
@@ -22,7 +32,7 @@ def is_two_pair(figure1, figure2, hand):
     >>> is_two_pair("3","2", ["4a", "3a", "2a", "2a"])     
     False
     """
-    if(figure1==figure2):
+    if(figure1 == figure2):
         return False
     return (is_pair(figure1, hand) and is_pair(figure2, hand))
 
@@ -35,7 +45,7 @@ def is_three(figure, hand):
     >>> is_three("3", ["3a", "3a", "6a", "3b"])     
     True
     """
-    return len([x for x in hand if x[:-1] == figure]) >=3
+    return len([x for x in hand if x[:-1] == figure]) == 3
 
 def is_straight(figure, hand):
     """
@@ -86,14 +96,14 @@ def is_full(figure1, figure2, hand):
 
 def is_four(figure, hand):
     """
-    >>> is_pair("3", ["3a", "3a", "3a", "3a"])     
+    >>> is_four("3", ["3a", "3a", "3a", "3a"])     
     True
-    >>> is_pair("2", ["3a", "3a", "3a", "3b"])     
+    >>> is_four("2", ["3a", "3a", "3a", "3b"])     
     False
-    >>> is_pair("3", ["Ka", "Ka", "6a", "6b"])     
+    >>> is_four("3", ["Ka", "Ka", "6a", "6b"])     
     False
     """
-    return len([x for x in hand if x[:-1] == figure]) >=4
+    return len([x for x in hand if x[:-1] == figure]) == 4
 
 def is_poker(figure, hand):
     """
@@ -107,30 +117,23 @@ def is_poker(figure, hand):
     hand_value = sorted([figures.index(cards[:-1]) for cards in hand])
     return is_flush(figure, hand) and is_straight(figure, hand) and figure == figures[max(hand_value)]
 
-def is_royal_poker(hand):
-    """
-    >>> is_royal_poker(["10a", "Ja", "Qa", "Ka", "9a"])
-    False
-    >>> is_royal_poker(["Aa", "Ka", "Qa", "Ja", "10a"])
-    True
-    """
-    hand_value = sorted([figures.index(cards[:-1]) for cards in hand])
-    
-    return is_flush("A", hand) and hand_value == [8,9,10,11,12]
-
+high_card = [partial(is_high_card, figure=f)for f in figures[::-1]]
 pair = [partial(is_pair, figure=f)for f in figures[::-1]]
 two_pair = [partial(is_two_pair, figure1=f1, figure2=f2)for f1 in figures[::-1] for f2 in figures[::-1]]
 three = [partial(is_three, figure=f)for f in figures[::-1]]
 straight = [partial(is_straight, figure=f)for f in figures[::-1]]
 kolor = [partial(is_flush, figure=f)for f in figures[::-1]]
-full = [partial(is_full, figure1=f1, figure2=f2)for f1 in figures[::-1] for f2 in figures[::-1]]
+full = [partial(is_full, figure1=f1, figure2=f2)for f2 in figures[::-1] for f1 in figures[::-1]]
 four = [partial(is_four, figure=f)for f in figures[::-1]]
 poker = [partial(is_poker, figure=f)for f in figures[::-1]]
-royal_poker = [partial(is_royal_poker)]
-strength = royal_poker + poker + four + full + kolor + straight + three + two_pair + pair
+strength = poker + four + full + kolor + straight + three + two_pair + pair + high_card
 
 def hand_value(cards):
     """
+    >>> hand_value(["2z", "3a", "Jq", "4w"]) > hand_value(["2z", "3a", "8q", "4w"]) 
+    True
+    >>> hand_value(["2z", "3a", "Jq", "4w"]) < hand_value(["2z", "2a", "5q", "4w"]) 
+    True
     >>> hand_value(["3z", "3a", "6q", "4w"]) > hand_value(["2z", "2a", "6q", "4w"]) 
     True
     >>> hand_value(["3z", "3a", "6q", "4w"]) < hand_value(["2z", "2a", "4q", "4w"])
@@ -140,6 +143,10 @@ def hand_value(cards):
     >>> hand_value(["2z", "3a", "4q", "5w"]) < hand_value(["2a", "4a", "5a", "2a"])
     True
     >>> hand_value(["2z", "2a", "3q", "3w", "3a"]) > hand_value(["2a", "4a", "5a", "2a"])
+    True
+    >>> hand_value(["2z", "2a", "3q", "3w", "3a"]) < hand_value(["4z", "4a", "3q", "3w", "3a"])
+    True
+    >>> hand_value(["Jz", "Ja", "3q", "3w", "3a"]) < hand_value(["4z", "4a", "5q", "5w", "5a"])
     True
     >>> hand_value(["2z", "2a", "3q", "3w", "3a"]) < hand_value(["2a", "2a", "2b", "2a"])
     True
